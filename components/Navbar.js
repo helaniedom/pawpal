08a41e7f9ef85460710e419cfe67cd262eb88e95
cresdom <domcres807@icloud.com>
2026-04-16 11:01:50 -0600

    interface changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -3,19 +3,19 @@ import Link from "next/link";
 export default function Navbar() {
     return (
         <nav>
-        <div className="nav-container">
-            <Link href="/" className="logo">
-            PawPal
-            </Link>
+            <div className="nav-container">
+                <Link href="/" className="logo">
+                    PawPal
+                </Link>
 
-            <div className="nav-links">
-            <Link href="/">Dashboard</Link>
-            <Link href="/pets">Pets</Link>
-            <Link href="/schedule">Schedule</Link>
-            <Link href="/add-pet">Add Pet</Link>
-            <Link href="/add-reminder">Add Reminder</Link>
+                <div className="nav-links">
+                    <Link href="/">Dashboard</Link>
+                    <Link href="/pets">Pets</Link>
+                    <Link href="/schedule">Schedule</Link>
+                    <Link href="/add-pet">Add Pet</Link>
+                    <Link href="/add-reminder">Add Reminder</Link>
+                </div>
             </div>
-        </div>
         </nav>
     );
 }
